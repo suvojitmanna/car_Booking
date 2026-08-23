323b867c5d6f48c325fc0db8e06f15291c045c23
Suvojit Manna <mannasuvojit84@gmail.com>
2026-08-23 06:37:12 +0530

    db connect successfully

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,38 @@
+import connectDb from "@/src/lib/db";
+import User from "@/src/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email, password } = await req.json();
+    await connectDb();
+    let user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { message: "email already exist!" },
+        { status: 400 },
+      );
+    }
+    if (password.length<6) {
+      return NextResponse.json(
+        { message: "password must be 6 character" },
+        { status: 400 },
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password,10)
+
+    user = await User.create({
+        name,email,password:hashedPassword
+    })
+    return NextResponse.json(
+        user,
+        { status: 200 },
+      );
+  } catch (error) {
+    return NextResponse.json(
+        { message: `register error ${error}` },
+        { status: 500 },
+      );
+  }
+}
---
+++ src/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Model } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password?: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+export default User;
